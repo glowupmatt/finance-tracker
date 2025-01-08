1b7fd03f7586e8b73e76b117a54adbe88635bd47
Matthew Nicholson <thematthewnicholson@gmail.com>
2025-01-07 20:13:57 -0800

    create user migration for db

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,49 @@
+import { AuthOptions } from "next-auth";
+import bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
---
+++ src/queries.ts
@@ -0,0 +1,106 @@
+// 1
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+// 2
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+// 3
+// ... you will write your Prisma Client queries here
+async function main() {
+  const user1Email = `alice${Date.now()}@prisma.io`;
+  const user2Email = `bob${Date.now()}@prisma.io`;
+
+  // Seed the database with users and posts
+  const user1 = await prisma.user.create({
+    data: {
+      email: user1Email,
+      hashedPassword: "password",
+      firstName: "Alice",
+    },
+  });
+  const user2 = await prisma.user.create({
+    data: {
+      email: user2Email,
+      name: "Bob",
+      posts: {
+        create: [
+          {
+            title: "Check out Prisma on YouTube",
+            content: "https://pris.ly/youtube",
+            published: true,
+          },
+          {
+            title: "Follow Prisma on Twitter",
+            content: "https://twitter.com/prisma/",
+            published: false,
+          },
+        ],
+      },
+    },
+    include: {
+      posts: true,
+    },
+  });
+  console.log(
+    `Created users: ${user1.name} (${user1.posts.length} post) and ${user2.name} (${user2.posts.length} posts) `
+  );
+
+  // Retrieve all published posts
+  const allPosts = await prisma.post.findMany({
+    where: { published: true },
+  });
+  console.log(`Retrieved all published posts: ${JSON.stringify(allPosts)}`);
+
+  // Create a new post (written by an already existing user with email alice@prisma.io)
+  const newPost = await prisma.post.create({
+    data: {
+      title: "Join the Prisma Discord community",
+      content: "https://pris.ly/discord",
+      published: false,
+      author: {
+        connect: {
+          email: user1Email,
+        },
+      },
+    },
+  });
+  console.log(`Created a new post: ${JSON.stringify(newPost)}`);
+
+  // Publish the new post
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: newPost.id,
+    },
+    data: {
+      published: true,
+    },
+  });
+  console.log(
+    `Published the newly created post: ${JSON.stringify(updatedPost)}`
+  );
+
+  // Retrieve all posts by user with email alice@prisma.io
+  const postsByUser = await prisma.post.findMany({
+    where: {
+      author: {
+        email: user1Email,
+      },
+    },
+  });
+  console.log(
+    `Retrieved all posts from a specific user: ${JSON.stringify(postsByUser)}`
+  );
+}
+
+// 4
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    // 5
+    await prisma.$disconnect();
+    process.exit(1);
+  });
