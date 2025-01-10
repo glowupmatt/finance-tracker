c0cb8e48d3b0d467281770305d1afbce0cae18e4
Matthew Nicholson <thematthewnicholson@gmail.com>
2025-01-10 15:25:29 -0800

    feat: add utility functions for fetching transactions, pots, budgets, and recurring payments; implement currency formatting and random color generation; define Pot type

---
+++ src/lib/fetchUserActions.ts
@@ -0,0 +1,38 @@
+export async function fetchTransactions() {
+  try {
+    const response = await fetch("/api/transactions");
+    const data = await response.json();
+    return data.transactions;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function fetchPots() {
+  try {
+    const response = await fetch("/api/pots");
+    const data = await response.json();
+    return data.pots;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchBudgets() {
+  try {
+    const response = await fetch("/api/budgets");
+    const data = await response.json();
+    return data.budgets;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchRecurringPayments() {
+  try {
+    const response = await fetch("/api/recurringPayments");
+    const data = await response.json();
+    return data.recurringPayments;
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ src/types/PotTypes.ts
@@ -0,0 +1,11 @@
+import { Transaction } from "@prisma/client";
+
+export type Pot = {
+  id: string;
+  title: string;
+  targetAmount: number;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+  transactions: Transaction[];
+};
